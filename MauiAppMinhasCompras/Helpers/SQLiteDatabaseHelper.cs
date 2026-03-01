ffcf15a64e46c62f6ace5d12f4ad635440a58154
Jefferson Vilela <Jefferson Vilela@DESKTOP-D5NELOG>
2026-03-01 01:49:21 -0300

    add recursos do app

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SQLite;
 using MauiAppMinhasCompras.Models;
+using AudioUnit;
 
 namespace MauiAppMinhasCompras.Helpers
 {
@@ -17,6 +18,40 @@ namespace MauiAppMinhasCompras.Helpers
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
         }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql ="Update Produto SET Descricao = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
+
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+
+        }
+
+        public Task<int> Delete(int id) 
+        {
+
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+              
+        }
+
+        public Task<List<Produto>> GetAll() 
+        { 
+        
+           return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q) 
+        {
+
+            string sql = "SELECT * Produto Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
+
 
     }
 }
