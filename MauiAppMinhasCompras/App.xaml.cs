a29359551466bd436d9eb9637feb6a0497bd8101
Jefferson Vilela <Jefferson Vilela@DESKTOP-D5NELOG>
2026-03-02 18:18:44 -0300

    add recursos do app

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,15 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+        // _db = Campo
+
+        public static SQLiteDatabaseHelper Db
+            // Db = Propriedade
+
         public App()
         {
             InitializeComponent();
