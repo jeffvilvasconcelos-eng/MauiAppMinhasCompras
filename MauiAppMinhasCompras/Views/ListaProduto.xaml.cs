6bd832d9f4186b3f423033c3f522c6cec53ffb9c
Jefferson Vilela <Jefferson Vilela@DESKTOP-D5NELOG>
2026-03-15 16:24:15 -0300

    add recursos  ao app

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -2,6 +2,7 @@ using MauiAppMinhasCompras.Models;
 using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
 using System;
+using System.Threading.Tasks;
 namespace MauiAppMinhasCompras.Views
 {
     public partial class ListaProduto : ContentPage
@@ -15,18 +16,25 @@ namespace MauiAppMinhasCompras.Views
         }
         protected async override void OnAppearing()
         {
-            List<Produto> temporario = await App.Db.GetAll();
-            temporario.ForEach(i => lista.Add(i));
-
             try
             {
-                Navigation.PushAsync(new Views.NovoProduto());
+                List<Produto> temporario = await App.Db.GetAll();
+                temporario.ForEach(i => lista.Add(i));
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Ops", ex.Message, "Ok");
-            }
-        }
+
+                try
+                {
+                    Navigation.PushAsync(new Views.NovoProduto());
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Ops", ex.Message, "Ok");
+                }
+
+
         private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
         {
             string q = e.NewTextValue;
@@ -37,24 +45,47 @@ namespace MauiAppMinhasCompras.Views
 
             temporario.ForEach(i => lista.Add(i));
         }
-        private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
         {
             double soma = lista.Sum(i => i.Total);
 
             string msg = $"O total é {soma:C}";
 
             DisplayAlert("Total dos Produtos", msg, "Ok");
-                  
+
         }
 
-        private void MenuItem_Clicked(object sender, EventArgs e)
+        private async Task MenuItem_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                MenuItem selecionado = sender as MenuItem;
+                Produto p = selecionado.BindingContext as Produto;
+                boll confirm = await DisplayAlest(
+                    "Tem Certeza?" "Remover Produto?","Sim", "Não");
+                if(confirm)
+                {
+                    await App.Db.Delete(p.Id);
+                    lista.Remove(p);
+                }
 
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+    
+            }
+        }
+
+        private void ToolbarItem_clicked(object sender, EventArgs e)
+        {
+            // Adicione aqui a lógica para o evento de adicionar produto
         }
     }
 
 
 
 
-    }
+}
 
