3de2eae6ef50ccafa21f0856d7dbc014e992d279
Jefferson Vilela <Jefferson Vilela@DESKTOP-D5NELOG>
2026-03-22 12:17:34 -0300

    add recursos ao app

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -19,7 +19,7 @@ public partial class ListaProduto : ContentPage
     {
         try
         {
-            lista.Clear(); 
+            lista.Clear();
             List<Produto> tmp = await App.Db.GetAll();
 
             tmp.ForEach(i => lista.Add(i));
@@ -29,7 +29,7 @@ public partial class ListaProduto : ContentPage
             await DisplayAlert("Ops", ex.Message, "OK");
         }
     }
-            private void ToolbarItem_Clicked(object sender, EventArgs e)
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
         try
         {
@@ -44,69 +44,96 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-       
+
         try
-        { 
-                String q = e.NewTextValue;
+        {
+            String q = e.NewTextValue;
 
-                lista.Clear();
 
-                List<Produto> tmp = await App.Db.Search(q);
+            lst_produtos.IsRefreshing = true;
+             
 
-                tmp.ForEach(i => lista.Add(i));
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
     }
         catch (Exception ex)
         {
             await DisplayAlert("Ops", ex.Message, "OK");
+}
+            finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
     }
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+{
+    try
     {
-        try
+        double soma = lista.Sum(i => i.Total);
+        string msg = $"O total é {soma:C}";
+    }
+    catch (Exception ex)
+    {
+        String Message = $"Ops, ocorreu um erro: {ex.Message}";
+        DisplayAlert("Total dos Produtos", Message, "OK");
+    }
+}
+
+private async void MenuItem_Clicked(object sender, EventArgs e)
+{
+    try
+    {
+        MenuItem Selecionado = sender as MenuItem;
+        Produto p = Selecionado.BindingContext as Produto;
+        bool confirm = await DisplayAlert("Tem Certeza?", $"Remover {p.Descricao}?", "sim", "Não");
+
+        if (confirm)
         {
-            double soma = lista.Sum(i => i.Total);
-            string msg = $"O total é {soma:C}";
-        }
-        catch (Exception ex)
-        {
-            String Message = $"Ops, ocorreu um erro: {ex.Message}";
-            DisplayAlert("Total dos Produtos", Message, "OK");
+            await App.Db.Delete(p.Id);
+            lista.Remove(p);
         }
     }
-
-    private async void MenuItem_Clicked(object sender, EventArgs e)
+    catch (Exception ex)
     {
-        try
-        {
-            MenuItem Selecionado = sender as MenuItem;
-            Produto p = Selecionado.BindingContext as Produto;
-            bool confirm = await DisplayAlert("Tem Certeza?", $"Remover {p.Descricao}?", "sim", "Não");
-
-            if (confirm)
-            {
-                await App.Db.Delete(p.Id);
-                lista.Remove(p);
-            }
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops", ex.Message, "OK");
-        }
+        await DisplayAlert("Ops", ex.Message, "OK");
     }
+}
 
-    private async void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+private async void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+{
+    try
     {
-        try
+        Produto p = e.SelectedItem as Produto;
+        Navigation.PushAsync(new Views.EditarProduto
         {
-            Produto p = e.SelectedItem as Produto;
-            Navigation.PushAsync(new Views.EditarProduto
-            {
-                BindingContext = p,
-            });
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops", ex.Message, "OK");
-        }
+            BindingContext = p,
+        });
     }
+    catch (Exception ex)
+    {
+        await DisplayAlert("Ops", ex.Message, "OK");
+    }
+}
+
+private async void lst_produtos_Refreshing(object sender, EventArgs e)
+{
+    try
+    {
+        lista.Clear();
+        List<Produto> tmp = await App.Db.GetAll();
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+    catch (Exception ex)
+    {
+        await DisplayAlert("Ops", ex.Message, "OK");
+    }
+    finally
+    {
+        lst_produtos.IsRefreshing = false;
+    }
+}
 }
